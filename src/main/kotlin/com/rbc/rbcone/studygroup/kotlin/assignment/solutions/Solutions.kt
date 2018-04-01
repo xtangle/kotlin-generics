9f341020e26c2a86239d5ebedf6b159027b2e1d7
xtangle <jackyx5724@hotmail.com>
2018-04-01 16:49:44 -0400

    Remove solutions from the master branch

---
+++ src/main/kotlin/com/rbc/rbcone/studygroup/kotlin/assignment/solutions/Solutions.kt
@@ -0,0 +1,9 @@
+package com.rbc.rbcone.studygroup.kotlin.assignment.solutions
+
+fun main(args: Array<String>) {
+
+    /**
+     * Solutions for each problem are in the 'solutions' branch.
+     */
+
+}
