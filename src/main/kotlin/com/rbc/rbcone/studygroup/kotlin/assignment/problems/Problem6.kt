89e7a86462955df681b2523c46ac8c1d2cedea84
xtangle <jackyx5724@hotmail.com>
2018-10-23 19:49:29 -0400

    Fix problem 6 test cases

--- src/main/kotlin/com/rbc/rbcone/studygroup/kotlin/assignment/problems/Problem6.kt
+++ src/main/kotlin/com/rbc/rbcone/studygroup/kotlin/assignment/problems/Problem6.kt
@@ -23,7 +23,11 @@ fun main(args: Array<String>) {
 
     /*
     luckyNumbers.take(10).toList().let { println(it); it shouldBe listOf(1, 3, 7, 9, 13, 15, 21, 25, 31, 33) }
-    mapOf(100 to 615, 1000 to 8827, 10000 to 115597).mapKeys { luckyNumbers.elementAt(it.key) }
+    luckyNumbers.take(57).toList() shouldBe listOf(1, 3, 7, 9, 13, 15, 21, 25, 31, 33, 37, 43, 49, 51, 63,
+        67, 69, 73, 75, 79, 87, 93, 99, 105, 111, 115, 127, 129, 133, 135, 141, 151, 159, 163, 169, 171, 189,
+        193, 195, 201, 205, 211, 219, 223, 231, 235, 237, 241, 259, 261, 267, 273, 283, 285, 289, 297, 303)
+
+    mapOf(100 to 613, 1000 to 8809, 10000 to 115591).mapKeys { luckyNumbers.elementAt(it.key - 1) }
         .forEach { actual, expected -> println(actual); actual shouldBe expected }
 
     val n = 100000
